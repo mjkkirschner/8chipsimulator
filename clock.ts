184f280b6fa452374b5a780e19720516118d3e97
Michael Kirschner <michaelkirschner@Michaels-MacBook-Pro.local>
2017-12-29 02:22:42 -0500

    runs - try using sub components inside component next-

--- clock.ts
+++ clock.ts
@@ -1,11 +1,9 @@
 
-
-
 /**
  * clock is a time based oscillator which provides a binary output pin - 
  * it also has a view which draws a timeSeries chart of the clock signal over time.
  */
-class clock { 
+class clock {
 
     private smoothieChart;
     private timeSeries;
@@ -24,7 +22,7 @@ class clock {
 
     public startClock() {
         setInterval(() => {
-            //set 0
+            //set 0  
             this.tick()
             //still zero at cycle *2
             setTimeout(() => { this.tick() }, this.cycle);
@@ -88,6 +86,7 @@ class toggleSwitch {
 
 /**
  * a flipFlop - this component has a single bit of memory, 3 inputs, and an output.
+ * It loads the input bit to the output if enable is true and if the clock moves from low to high.
  */
 class binaryCell {
 
@@ -125,7 +124,49 @@ class binaryCell {
         //TODO is this correct?
         this.outputPin.value = this.state;
         this.timeSeries.append(new Date().getTime(), this.outputPin.value);
-        console.log("register state is",this.state);
+        console.log("register state is", this.state);
+        this.lastClockpinValue = this.clockPin.value;
+
+    }
+
+}
+class byteRegister {
+
+    public clockPin: pin;
+    public dataPins: pin[] = [];
+    public outputPins: pin[] = _.range(0, 8).map(x => { return new pin() });
+    public enablePin: pin;
+
+    private state: boolean[] = _.range(0, 8).map(x => { return false });
+    private lastClockpinValue;
+    private smoothieChart;
+    private timeSeries;
+
+
+    constructor(signalDrawing: HTMLCanvasElement) {
+        this.smoothieChart = new SmoothieChart({ maxValueScale: 1.5, interpolation: 'step' });
+        this.smoothieChart.streamTo(signalDrawing);
+        this.timeSeries = new TimeSeries();
+        this.smoothieChart.addTimeSeries(this.timeSeries);
+    }
+
+    public update() {
+
+        let clockPinValue = this.clockPin.value;
+
+        //when the clock pin goes from false to true, and the enable pin is true:
+        //set the current input to the output and update the state to that value.
+
+        if ((clockPinValue == true && this.lastClockpinValue == false) && this.enablePin.value == true) {
+            //set each state to the input pin value.
+            this.dataPins.forEach((pin, index) => { this.state[index] = pin.value });
+        }
+        this.outputPins.forEach((pin, index) => { pin.value = this.state[index] });
+
+        var outputAsInt = parseInt(this.outputPins.map(pin => { return Number(pin.value) }).join(""), 2);
+        this.timeSeries.append(new Date().getTime(), outputAsInt);
+
+        console.log("register state is", outputAsInt);
         this.lastClockpinValue = this.clockPin.value;
 
     }
--- main.ts
+++ main.ts
@@ -1,12 +1,20 @@
 
+
 var clockComponent = new clock(document.getElementById("timeline") as HTMLCanvasElement,200);
-var register = new binaryCell(document.getElementById("timeline2") as HTMLCanvasElement);
+var register = new byteRegister(document.getElementById("timeline2") as HTMLCanvasElement);
 var enableButton = new toggleSwitch(document.getElementById("componentsCanvas"),"enable");
-var dataButton = new toggleSwitch(document.getElementById("componentsCanvas"),"data");
+
+var dataButton = new toggleSwitch(document.getElementById("componentsCanvas"),"data128");
+var dataButton2 = new toggleSwitch(document.getElementById("componentsCanvas"),"data64");
+
+
+
+
 
 register.clockPin = clockComponent.outputPin;
 register.enablePin = enableButton.outputPin;
-register.dataPin = dataButton.outputPin;
+register.dataPins[0] = dataButton.outputPin;
+register.dataPins[1] = dataButton2.outputPin;
 
 //TODO - this will probably not work for more complex circuits - 
 //may need to be evaluated in topological order in a delay based manner...
